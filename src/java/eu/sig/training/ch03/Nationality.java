5118a5e3dc9702715d6b7afcc3d2b8d54c04e643
Michiel Flinkenflogel <mflinkenflogel@outlook.com>
2021-05-18 13:12:51 +0200

    -Enum Nationality now has colours as extra parameter. -Flag factory returns colors from constructed flag with Nationality enum.

--- src/java/eu/sig/training/ch03/Nationality.java
+++ src/java/eu/sig/training/ch03/Nationality.java
@@ -1,5 +1,28 @@
 package eu.sig.training.ch03;
 
+import java.awt.*;
+
 public enum Nationality {
-    DUTCH, GERMAN, BELGIAN, LUXEMBOURGER, FRENCH, ITALIAN, ROMANIA, IRELAND, HUNGARIAN, BULGARIAN, RUSSIA, UNCLASSIFIED
+    DUTCH(Color.RED, Color.WHITE, Color.BLUE),
+    GERMAN(Color.BLACK, Color.RED, Color.YELLOW),
+    BELGIAN(Color.BLACK, Color.YELLOW, Color.RED),
+    LUXEMBOURGER(Color.RED, Color.WHITE, Color.BLUE),
+    FRENCH(Color.BLUE, Color.WHITE, Color.RED),
+    ITALIAN(Color.GREEN, Color.WHITE, Color.RED),
+    ROMANIA(Color.BLUE, Color.YELLOW, Color.RED),
+    IRELAND(Color.GREEN, Color.WHITE, Color.ORANGE),
+    HUNGARIAN(Color.RED, Color.WHITE, Color.GREEN),
+    BULGARIAN(Color.WHITE, Color.GREEN, Color.RED),
+    RUSSIA(Color.WHITE, Color.BLUE, Color.RED),
+    UNCLASSIFIED(Color.GRAY);
+
+    private Flag flag;
+
+    Nationality(Color...colors) {
+        this.flag = new Flag(colors);
+    }
+
+    public Flag getFlag() {
+        return flag;
+    }
 }
